1292e56a56813377cf30b8fa271cbd6f08192b2b
novellgeek <phil@holdstock.co.nz>
2025-11-22 11:32:40 +1300

    Add files via upload

---
+++ tawhiri/dashboard/assets/images/__init__.py
@@ -0,0 +1,16 @@
+"""
+Tawhiri - Space Domain Awareness System
+========================================
+
+A modular space domain awareness system for NZDF.
+
+Modules:
+    - space_weather: Space weather monitoring and forecasting
+    - orbit_viz: Satellite orbit visualization and tracking
+
+Author: NZDF Space Domain Awareness Team
+Version: 2.0.0 (Modular)
+"""
+
+__version__ = "2.0.0"
+__author__ = "NZDF Space Domain Awareness Team"
